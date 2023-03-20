63fb318a13ccbc0439dad25f6d78801f50ecd4e9
MrLumus <ivanbat.dev@gmail.com>
2023-03-20 19:20:38 +0300

    added wow.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgwWowService } from 'ngx-wow';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wedding-invitation';
+
+  constructor(private wowService: NgwWowService) {}
+
+  ngOnInit() {
+    this.wowService.init();
+  }
+
+  reset() {
+    this.wowService.init();
+  }
 }
